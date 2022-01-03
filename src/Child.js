bd6d9efbabfc847bc16bc3d136a82ab18ca2137d
JazzPants <leighton.l14@gmail.com>
2022-01-03 23:24:43 +1100

    Done!

--- src/Child.js
+++ src/Child.js
@@ -3,13 +3,27 @@ import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div
+      <div onClick={() => this.props.handleColorChange(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
-      ></div>
+        style={{backgroundColor: this.props.color}}>
+
+        </div>
     )
   }
 }
+//LAB NOTE:
+//Now that the function colorChange takes in an argument, we'll need to update the onClick of Child to be
+//a function that invokes this.props.handleColorChange and passes in a random color as the argument:
+
+
+//with out () => "we get maximum depth reached" - infinite render loop
+//function is immediately called/invoked! First we need to refer to the callback function through an event/change handler function (onClick={()=>...})
+//(in this case an anonymous function() => ),  to stop the instant looping!
+
+//call the handleColorChange prop passed from "Parent.js" by using this.props.handleColorChange
+//console.log(this.props) this.props is an Object function
+//this.props.handleColorChange = {this.changeColor} !! which invokes changeColor function
 
 export default Child
--- src/Parent.js
+++ src/Parent.js
@@ -7,18 +7,28 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
   }
 
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
+  }
+
+  //pass state changing function as a PROP to the child component (make it an attribute!)
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
 }
+//declaring props, color and handleColorChange in this top level component "Parent.js"
 
 export default Parent
